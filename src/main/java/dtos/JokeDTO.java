523c1d1f0210d66b2d548a765fecf7b5177d222f
Claes <claesvonh@gmail.com>
2020-09-16 16:59:44 +0200

    Car and joke added

---
+++ src/main/java/dtos/JokeDTO.java
@@ -0,0 +1,39 @@
+package dtos;
+
+import entities.Joke;
+
+public class JokeDTO {
+    
+    private int id;
+    private String theJoke;
+    private String type;
+        
+    public JokeDTO(int id, String name, String studentID) {
+        this.id = id;
+        this.theJoke = theJoke;
+        this.type = type;
+    }
+    
+    public JokeDTO(Joke joke) {
+        this.id = joke.getId();
+        this.theJoke = joke.getTheJoke();
+        this.type = joke.getType();
+    }
+
+    public String getTheJoke() {
+        return theJoke;
+    }
+
+    public void setTheJoke(String theJoke) {
+        this.theJoke = theJoke;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+       
+}
