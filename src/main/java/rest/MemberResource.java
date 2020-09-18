2af90d392b1c405648a9ba4c1d5a7f1582a284a2
Claes <claesvonh@gmail.com>
2020-09-18 08:56:11 +0200

    path MemeberResource fixed

--- src/main/java/rest/MemberResource.java
+++ src/main/java/rest/MemberResource.java
@@ -12,7 +12,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 
-@Path("members")
+@Path("groupmembers/all")
 public class MemberResource {
 
     private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory();
